1ab673a1fdf8857e7051ff91bd5ba601f8180ddc
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-29 00:36:43 +0300

    feat: Add update balance DTO and endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,50 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { TradeModule } from './trade/trade.module';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { GlobalResponseInterceptor } from './infrastructure/middlewares/global-response.handler';
+import { GlobalExceptionFilter } from './infrastructure/middlewares/global-error.handler';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ShareModule } from './share/share.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, PortfolioModule, TradeModule, ShareModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 100,
+    }]),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    JwtModule
+
+
+
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    ConfigService,
+
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: GlobalResponseInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, VersioningType } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // somewhere in your initialization file
+  app.use(compression());
+  app.use(helmet());
+
+
+
+
   await app.listen(3000);
+  Logger.debug(`Server running on http://localhost:3000`, 'Bootstrap');
 }
 bootstrap();
